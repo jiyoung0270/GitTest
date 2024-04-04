87ae8dabfea8fff513246f0f32a4c56ba92c673b
jiyoung0270 <jiyoung0270@naver.com>
2024-04-04 19:48:46 +0900

    add calendar

---
+++ app/src/main/java/eu/tutorials/practiceapp/HorizontalCalendar.kt
@@ -0,0 +1,154 @@
+package eu.tutorials.practiceapp
+
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.format.TextStyle
+import java.util.Locale
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HorizontalCalendar(
+    modifier: Modifier = Modifier,
+    currentDate: LocalDate = LocalDate.now(),
+    config: HorizontalCalendarConfig = HorizontalCalendarConfig(),
+    onSelectedDate: (LocalDate) -> Unit
+) {
+    //page는 0부터 시작하기 때문에 getMonthValue - 1을 해줘야 함
+    val initialPage = (currentDate.year - config.yearRange.first) * 12 + currentDate.monthValue - 1
+    var currentSelectedDate by remember { mutableStateOf(currentDate) }
+    var currentMonth by remember { mutableStateOf(YearMonth.now()) }
+    var currentPage by remember { mutableStateOf(initialPage) }
+    val pagerState = rememberPagerState(initialPage = initialPage)
+
+    LaunchedEffect(pagerState.currentPage) {
+        val addMonth = (pagerState.currentPage - currentPage).toLong()
+        currentMonth = currentMonth.plusMonths(addMonth)
+        currentPage = pagerState.currentPage
+    }
+
+    LaunchedEffect(currentSelectedDate) {
+        onSelectedDate(currentSelectedDate)
+    }
+
+    Column(modifier = modifier) {
+        val headerText = currentMonth.dateFormat("yyyy년 M월")
+        val pageCount = (config.yearRange.last - config.yearRange.first) * 12
+        CalendarHeader(
+            modifier = Modifier.padding(20.dp),
+            text = headerText
+        )
+        HorizontalPager(
+            pageCount = pageCount,
+            state = pagerState
+        ) { page ->
+            val date = LocalDate.of(
+                config.yearRange.first + page / 12,
+                page % 12 + 1,
+                1
+            )
+            if (page in pagerState.currentPage - 1..pagerState.currentPage + 1) { // 페이징 성능 개선을 위한 조건문
+                CalendarMonthItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp),
+                    currentDate = date,
+                    selectedDate = currentSelectedDate,
+                    onSelectedDate = { date ->
+                        currentSelectedDate = date
+                    }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CalendarHeader(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Box(modifier = modifier) {
+        Text(
+            text = text,
+            //style = BlackN12
+        )
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun CalendarMonthItem(
+    modifier: Modifier = Modifier,
+    currentDate: LocalDate,
+    selectedDate: LocalDate,
+    onSelectedDate: (LocalDate) -> Unit
+) {
+    val lastDay by remember { mutableStateOf(currentDate.lengthOfMonth()) }
+    val firstDayOfWeek by remember { mutableStateOf(currentDate.dayOfWeek.value) }
+    val days by remember { mutableStateOf(IntRange(1, lastDay).toList()) }
+
+    Column(modifier = modifier) {
+
+        LazyVerticalGrid(
+            modifier = Modifier.height(260.dp),
+            columns = GridCells.Fixed(7)
+        ) {
+            for (i in 1 until firstDayOfWeek) { // 처음 날짜가 시작하는 요일 전까지 빈 박스 생성
+                item {
+                    Box(
+                        modifier = Modifier
+                            .size(30.dp)
+                            .padding(top = 10.dp)
+                    )
+                }
+            }
+            items(days) { day ->
+                val date = currentDate.withDayOfMonth(day)
+                val isSelected = remember(selectedDate) {
+                    selectedDate.compareTo(date) == 0
+                }
+                CalendarDay(
+                    modifier = Modifier.padding(top = 10.dp),
+                    date = date,
+                    isToday = date == LocalDate.now(),
+                    isSelected = isSelected,
+                    onSelectedDate = onSelectedDate
+                )
+            }
+        }
+    }
+}
+
--- app/src/main/java/eu/tutorials/practiceapp/Todo.kt
+++ app/src/main/java/eu/tutorials/practiceapp/Todo.kt
@@ -1,11 +1,11 @@
 package eu.tutorials.practiceapp
 
 data class Todo(
-    var text: String = "",
-    var tag: String = "",
-    var time: String = "",
-    var alarm: String = "",
-    var period: String = "",
+    var text: String,
+    var tag: String,
+    var time: String,
+    var alarm: String,
+    var period: String,
     var repeat: Int,
     var done: Boolean = false,
     var onEdit: Boolean = true
